d8ee04df312683a04d2b88e242517b7544dc4a35
Patrick J Cardoso <patrickjcardoso@gmail.com>
2023-05-26 14:56:39 -0300

    Adicionado Banco de dados Mongo

--- TaskMaster/app.py
+++ TaskMaster/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
 
 app = Flask(__name__, static_url_path='/static')
 
-tasks = []
+# Conecte-se ao servidor do MongoDB
+client = MongoClient('mongodb://localhost:27017')
+
+# Acesse o banco de dados
+db = client['taskmaster']
+
+# Acesse a coleção (tabela) de tarefas
+tasks_collection = db['tasks']
 
 @app.route('/')
 def index():
+    # Obtenha todas as tarefas da coleção
+    tasks = tasks_collection.find()
     return render_template('index.html', tasks=tasks)
 
 @app.route('/create_task', methods=['POST'])
@@ -14,28 +24,24 @@ def create_task():
     description = request.form.get('description')
     due_date = request.form.get('due_date')
 
+    # Insira a nova tarefa na coleção
     task = {'title': title, 'description': description, 'due_date': due_date}
-    tasks.append(task)
+    tasks_collection.insert_one(task)
 
     return redirect('/')
 
-@app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
+@app.route('/edit_task/<string:task_id>', methods=['GET', 'POST'])
 def edit_task(task_id):
-    # Subtrai 1 do ID da tarefa para obter o índice correto na lista
-    task_index = task_id - 1
-
-    # Verifica se o índice está dentro dos limites da lista de tarefas
-    if 0 <= task_index < len(tasks):
-        task = tasks[task_index]
-
+    # Verifique se a tarefa existe na coleção
+    task = tasks_collection.find_one({'_id': task_id})
+    if task:
         if request.method == 'POST':
             title = request.form.get('title')
             description = request.form.get('description')
             due_date = request.form.get('due_date')
 
-            task['title'] = title
-            task['description'] = description
-            task['due_date'] = due_date
+            # Atualize a tarefa na coleção
+            tasks_collection.update_one({'_id': task_id}, {'$set': {'title': title, 'description': description, 'due_date': due_date}})
 
             return redirect('/')
 
@@ -43,16 +49,12 @@ def edit_task(task_id):
 
     return redirect('/')
 
-@app.route('/delete_task/<int:task_id>', methods=['GET'])
+@app.route('/delete_task/<string:task_id>', methods=['GET'])
 def delete_task(task_id):
-    # Subtrai 1 do ID da tarefa para obter o índice correto na lista
-    task_index = task_id - 1
+    # Remova a tarefa da coleção
+    tasks_collection.delete_one({'_id': task_id})
 
-    if 0 <= task_index < len(tasks):
-        del tasks[task_index]
-        
     return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
